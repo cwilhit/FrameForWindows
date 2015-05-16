c9ab73544247d35015a8a2352dd4c6fd558b1f1b
Craig Wilhite <cwilhit@purdue.edu>
2015-05-16 15:52:25 -0400

    Setting up project

---
+++ Frame for WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Frame_for_WP.Resources;
+
+namespace Frame_for_WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
